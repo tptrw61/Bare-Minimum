09081e8e5e81ff22d3338ce82bccc63262fb40aa
cwahl1 <35500406+cwahl1@users.noreply.github.com>
2019-03-30 14:19:10 -0700

    move

---
+++ Game Jam Spring 2019/Assets/scripts/MoveCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    public float margin = 0.05f;
+    public float leftBound = 0;
+    public float rightBound = 0;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float normalizedMouseX = Input.mousePosition.x / Screen.width;
+
+        //movement left
+        if (transform.position.x < leftBound)
+        {
+            if(normalizedMouseX > margin)
+            {
+                transform.position += new Vector3(speed * Time.deltaTime, 0,0); // move on +X axis
+            }
+        }
+
+        //movement right
+        if (transform.position.x > rightBound)
+        {
+            if (normalizedMouseX < 1-margin)
+            {
+                transform.position -= new Vector3(speed * Time.deltaTime, 0, 0); // move on +X axis
+            }
+        }
+    }
+}
