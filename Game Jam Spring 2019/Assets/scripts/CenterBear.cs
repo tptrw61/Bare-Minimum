bbc7958b2a7f7314eff3bb8ddf1811fd349f794e
cwahl1 <35500406+cwahl1@users.noreply.github.com>
2019-03-30 16:37:09 -0700

    things are lookn pretty

--- Game Jam Spring 2019/Assets/scripts/CenterBear.cs
+++ Game Jam Spring 2019/Assets/scripts/CenterBear.cs
@@ -11,7 +11,7 @@ public class CenterBear : MonoBehaviour
     void Update()
     {
         Quaternion rotation = myCamera.transform.rotation;
-        Vector3 newposition = myCamera.transform.position + (rotation * Vector3.forward * distance) + (Vector3.down * 3) + (Vector3.right * 6.5f);
+        Vector3 newposition = myCamera.transform.position + (rotation * Vector3.forward * distance) + (Vector3.down * 3) + (Vector3.left * 7);
         this.transform.position = Vector3.Lerp(transform.position, newposition, Time.deltaTime * speed);
     }
 }
