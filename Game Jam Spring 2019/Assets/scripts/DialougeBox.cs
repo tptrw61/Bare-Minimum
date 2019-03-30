080d2d9854cb4d3bc02c1bd17ccdbc68cd049cd7
cwahl1 <35500406+cwahl1@users.noreply.github.com>
2019-03-30 15:50:52 -0700

    text box hidden

--- Game Jam Spring 2019/Assets/scripts/DialougeBox.cs
+++ Game Jam Spring 2019/Assets/scripts/DialougeBox.cs
@@ -7,12 +7,22 @@ public class DialougeBox : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Show()
+    {
+        this.GetComponent<Renderer>().enabled = true;
+    }
+
+    void Hide()
+    {
+        this.GetComponent<Renderer>().enabled = false;
     }
 }
