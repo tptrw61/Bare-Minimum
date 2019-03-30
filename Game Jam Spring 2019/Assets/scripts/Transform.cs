b4597deaded1e6677396e975ee9a47876b1c0174
cwahl1 <35500406+cwahl1@users.noreply.github.com>
2019-03-30 13:46:24 -0700

    boss moves

---
+++ Game Jam Spring 2019/Assets/scripts/Transform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform : MonoBehaviour
+{
+    private Vector3 targetpos;
+    public float distance;
+
+    public float speed;
+    private bool showing = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = Vector3.left * distance;
+        Show();
+    }
+
+    void Show()
+    {
+        this.targetpos = new Vector3(8.6f, .5f, 0f);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetpos, Time.deltaTime*speed);
+        if(showing && Vector3.Distance(transform.position, targetpos)<= 0.1f)
+        {
+            showDialogue();
+        }
+    }
+
+    private void showDialogue()
+    {
+
+    }
+
+    void Hide()
+    {
+        this.targetpos = new Vector3(2.6f, .5f, 0f);
+    }
+}
