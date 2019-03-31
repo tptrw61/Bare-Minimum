44f34e4dc4c8c693accdaa541035f4342187e250
tptrw61 <tptrw.61@gmail.com>
2019-03-30 17:11:54 -0700

    Update Stamina.cs

--- Game Jam Spring 2019/Assets/scripts/Stamina.cs
+++ Game Jam Spring 2019/Assets/scripts/Stamina.cs
@@ -8,7 +8,7 @@ public class Stamina : MonoBehaviour
 
     private Vector3 prevPos;
 
-    public float staminaDrain = .125f, stressDrain = 1f;
+    public float staminaDrain = 1f, stressDrain = 1f;
 
     public float staminaMax = 3000f;
     public float staminaCurrent;
@@ -33,15 +33,17 @@ public class Stamina : MonoBehaviour
     {
         Vector3 curPos = Input.mousePosition;
         staminaCurrent -= (Vector3.Distance(curPos, prevPos)*staminaDrain) + (stressDrain*Time.deltaTime*stressCurrent);
+        if (staminaCurrent < 0)
+            staminaCurrent = 0;
         prevPos = curPos;
         //DEBUG ONLY
         if (DEBUG)
         {
-            if (Input.GetKeyDown(KeyCode.Plus))
-                    increaseStress();
-            if (Input.GetKeyDown(KeyCode.Minus))
-                stressCurrent--;
-            if (Input.GetKeyDown(KeyCode.Asterisk))
+            if (Input.GetKeyDown(KeyCode.I))
+                increaseStress();
+            if (Input.GetKeyDown(KeyCode.O))
+                decreaseStress();
+            if (Input.GetKeyDown(KeyCode.P))
             {
                 staminaCurrent = staminaMax;
                 stressCurrent = 0;
@@ -67,10 +69,14 @@ public class Stamina : MonoBehaviour
     public void useStamina(float used)
     {
         staminaCurrent -= used;
+        if (staminaCurrent < 0)
+            staminaCurrent = 0;
     }
     public void restoreStamina(float restored)
     {
         staminaCurrent += restored;
+        if (staminaCurrent > staminaMax)
+            staminaCurrent = staminaMax;
     }
 
     public void increaseStress()
