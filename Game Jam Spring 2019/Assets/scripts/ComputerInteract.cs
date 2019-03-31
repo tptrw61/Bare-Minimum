8714bfa98ecc0184c03de9d6f13b23338d9a251e
isabela-hutchings <cssetton@email.arizona.edu>
2019-03-30 19:35:50 -0700

    Aded a interactive script to the computer

---
+++ Game Jam Spring 2019/Assets/scripts/ComputerInteract.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Yarn.Unity;
+
+public class ComputerInteract : MonoBehaviour
+{
+
+
+public class Computer : MonoBehaviour
+{
+    //public BoxCollider2D box;
+   // public DialogueRunner run;
+    private int count =0;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+                Debug.Log("touched the computer");
+                
+        //run.StartDialogue();
+    }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
