3ae72778317635f968aef7ea58826eb3bc745901
cwahl1 <35500406+cwahl1@users.noreply.github.com>
2019-03-30 17:09:19 -0700

    coffee done

---
+++ Game Jam Spring 2019/Assets/scripts/Coffee_interact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Coffee_interact : MonoBehaviour
+{
+    public Stamina stamina;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        stamina.restoreStamina(150f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
