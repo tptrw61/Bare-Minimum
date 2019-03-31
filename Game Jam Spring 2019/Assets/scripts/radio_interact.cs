f24dd99d35769ed07845cb9d216a98602ee64f38
cwahl1 <35500406+cwahl1@users.noreply.github.com>
2019-03-30 19:01:31 -0700

    fixed music

---
+++ Game Jam Spring 2019/Assets/scripts/radio_interact.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radio_interact : MonoBehaviour
+{
+    public AudioSource music;
+    public AudioSource office;
+    bool on;
+    // Start is called before the first frame update
+    void Start()
+    {
+        on = false;
+    }
+
+    void OnMouseDown()
+    {
+        if (!on)
+        {
+            music.Play();
+            office.Stop();
+            on = true;
+        }
+        else
+        {
+            music.Stop();
+            office.Play();
+            on = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
