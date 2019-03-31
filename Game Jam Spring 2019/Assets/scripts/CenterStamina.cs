86e3f610c98c62924b9c328fe3ca6ebcab1c10f3
cwahl1 <35500406+cwahl1@users.noreply.github.com>
2019-03-31 13:00:26 -0700

    stamina stays with camera

---
+++ Game Jam Spring 2019/Assets/scripts/CenterStamina.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterStamina : MonoBehaviour
+{
+    public GameObject myCamera;
+    public float distance = 5.0f;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rotation = myCamera.transform.rotation;
+        Vector3 newposition = myCamera.transform.position + (rotation * Vector3.forward * distance) + (Vector3.up * 3.5f) + (Vector3.right * 7);
+        this.transform.position = Vector3.Lerp(transform.position, newposition, Time.deltaTime * speed);
+    }
+}
