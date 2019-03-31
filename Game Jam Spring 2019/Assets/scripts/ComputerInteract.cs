68d156413d3d8e9fde1636be9360dc6e89fb31c3
cwahl1 <35500406+cwahl1@users.noreply.github.com>
2019-03-30 22:16:40 -0700

    done

--- Game Jam Spring 2019/Assets/scripts/ComputerInteract.cs
+++ Game Jam Spring 2019/Assets/scripts/ComputerInteract.cs
@@ -8,11 +8,20 @@ public class ComputerInteract : MonoBehaviour
 
    public DialogueRunner run;
    //private int count =0;
+
+    public AudioSource beep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beep = GetComponent<AudioSource>();
+    }
     // Start is called before the first frame update
     // Update is called once per frame
     void OnMouseDown()
     {
         run.StartDialogue();
+        beep.Play();
     }
 
     // Update is called once per frame
